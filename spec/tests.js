6d7f7a3b81e453ac2c36cd176144080d6a7c51af
Anton <astandrik@gmail.com>
2015-09-26 01:53:08 +0300

    working perceptron

--- spec/tests.js
+++ spec/tests.js
@@ -4,7 +4,7 @@ describe("LinearCombination", function() {
      var a = [1,2,3,4];
      var b = [1,2,3,4];
      var sum = 0;
-     for(var i = 0; i < a.length; i++) sum += a[i] * b[i];  
+     for(var i = 0; i < a.length; i++) sum += a[i] * b[i];
      expect(N.sumNet(a,b)).toEqual(sum);
   });
 });
@@ -14,7 +14,7 @@ describe("Simple One-neuron Perceptron", function() {
      var P = new perceptron(1, 4);
      var b = [1,2,3,4];
      var sum = 0;
-     for(var i = 0; i < b.length; i++) sum +=  b[i];  
+     for(var i = 0; i < b.length; i++) sum +=  b[i];
      var result = P.process(b);
      result = result[0];
      expect(result).toEqual((10 > 10) ? 1 : 0);
@@ -59,12 +59,99 @@ describe("Perceptron", function() {
   });
 });
 
+function dec2bin(dec){
+    return (dec >>> 0).toString(2);
+}
+
+function getRandomArbitary(min, max)
+{
+  return Math.random() * (max - min) + min;
+}
+
 describe("Perceptron", function() {
   it("Try simple training set", function() {
-      debugger;
-     var set = [{in: [0,0,0], out: 1}, {in: [0,0,1], out: 0}, {in: [0,1,0], out: 1}, {in: [0,1,1], out:0},  {in: [1,0,0], out:1}, {in: [1,0,1], out:0}];
+     var set = [{in: [1,1,1], out: 1}, {in: [1,1,2], out: 0}, {in: [1,2,1], out: 1}, {in: [1,2,2], out:0},  {in: [2,1,1], out:1}, {in: [2,1,2], out:0}];
      var perc = new perceptron(1,3);
      train(perc, set);
-     expect(perc.process([1,0,1])).toEqual([0]);
+     expect(perc.process([2,1,2])).toEqual([0]);
+  });
+});
+
+describe("Perceptron", function() {
+  it("Try another training set", function() {
+    debugger;
+     var set = [{"in":[1,1,1,2],"out":[1]},{"in":[1,2,1,1],"out":[1]},{"in":[2,1,1,1],"out":[1]},{"in":[1,2,2,1],"out":[1]},{"in":[1,1,1,2],"out":[1]}];
+     var perc = new perceptron(1,4);
+     train(perc, set);
+     expect(perc.process([1,2,1,1])).toEqual([1]);
+  });
+});
+
+describe("Perceptron", function() {
+  it("Random set self check", function() {
+     var set = [];
+     var divider = getRandomArbitary(1, 3) >> 0;
+     var num;
+     var res;
+     var trueResults = [];
+     var tests = [];
+     for(var i = 0; i < 50; i++) {
+        num = getRandomArbitary(1, 10) >> 0;
+        res = (num % divider) === 0;
+        num = dec2bin(num);
+        num = num.split('').map(function(item) {return parseInt(item);});
+        for(var k = 0; k < num.length; k++) {
+          num[k] += 1;
+        }
+        trueResults.push([+res]);
+        tests.push(num);
+        set.push({in: num, out: [+res]});
+     }
+     var perc = new perceptron(1,4);
+     train(perc, set);
+     var results = [];
+
+     for(var j = 0; j < 50; j++) {
+        results.push(perc.process(tests[j]));
+     }
+     expect(results).toEqual(trueResults);
+  });
+});
+
+describe("Perceptron", function() {
+  it("Pure Random check", function() {
+     var set = [];
+     var divider = getRandomArbitary(1, 3) >> 0;
+     var num;
+     var res;
+     var trueResults = [];
+     var tests = [];
+     for(var i = 0; i < 100; i++) {
+        num = getRandomArbitary(1, 1000) >> 0;
+        res = (num % divider) === 0;
+        num = dec2bin(num);
+        num = num.split('').map(function(item) {return parseInt(item);});
+        for(var k = 0; k < num.length; k++) {
+          num[k] += 1;
+        }
+        trueResults.push([+res]);
+        set.push({in: num, out: [+res]});
+     }
+     var perc = new perceptron(1,20);
+     train(perc, set);
+     var results = [];
+     trueResults = [];
+     for(var j = 0; j < 10; j++) {
+       num = getRandomArbitary(1, 1000) >> 0;
+       res = (num % divider) === 0;
+       num = dec2bin(num);
+       num = num.split('').map(function(item) {return parseInt(item);});
+       for(var k = 0; k < num.length; k++) {
+         num[k] += 1;
+       }
+       trueResults.push([+res]);
+       results.push(perc.process(num));
+     }
+     expect(results).toEqual(trueResults);
   });
 });
--- src/perceptron.js
+++ src/perceptron.js
@@ -1,123 +1,138 @@
 
 var neuron = function() {
-    this.threshold = 10;
-    this.sumNet = function(vector, weights) {
-        var sum = 0;
-        vector.forEach(function(item, i) {
-            sum += item * weights[i];
-        });
-        return sum;
-    }
-    this.activeFunctionOut = function(input) {
-        if(input > this.threshold) return 1;
-        else return 0;
-    }
-    this.process = function(invector, inweights) {
-        var sum = this.sumNet(invector, inweights);
-        return this.activeFunctionOut(sum);
-    }
-    return this;
-}
-
+  this.threshold = 10;
+  this.sumNet = function(vector, weights) {
+    var sum = 0;
+    vector.forEach(function(item, i) {
+      sum += item * weights[i];
+    });
+    return sum;
+  };
+  this.activeFunctionOut = function(input) {
+    if (input > this.threshold) {return 1;} else {return 0;}
+  };
+  this.process = function(invector, inweights) {
+    var sum = this.sumNet(invector, inweights);
+    return this.activeFunctionOut(sum);
+  };
+  return this;
+};
 
 var perceptron = function(neuronsNumber, inVectorLength) {
-    this.neurons = [];
-    this.distributionMatrix = [];
-    while(neuronsNumber-- > 0) {
-        this.neurons.push(new neuron());    
-        var weights = [];
-        while(inVectorLength-- > 0) {
-            weights.push(1);
-        }
-        this.distributionMatrix.push(weights);
+  this.neurons = [];
+  this.distributionMatrix = [];
+  this.counter = 0;
+  this.inVectorLength = inVectorLength;
+  while (neuronsNumber-- > 0) {
+    this.neurons.push(new neuron());
+    var weights = [];
+    while (inVectorLength-- > 0) {
+      weights.push(1);
     }
-    this.process = function(vector) {
-        var resVector = [];
-        var self = this;
-        this.neurons.forEach(function(item, i){
-            resVector.push(item.process(vector, self.distributionMatrix[i]));
-        });
-        return resVector;
+    this.distributionMatrix.push(weights);
+  }
+  this.process = function(vector) {
+    var resVector = [];
+    var self = this;
+    while (vector.length < this.inVectorLength) {
+      vector.unshift(1);
     }
-    this.modifyWeights = function (input, errors, teta) {
-        var self = this;
-        errors.forEach(function(error, i) {
-            self.distributionMatrix.forEach(function(elem, j) {
-                elem.forEach(function(oldWeight, k) {
-                    self.distributionMatrix[j][k] = oldWeight + teta * error[0] * input[i][k];
-                });
-            });
-        });   
-    }
-    return this;
-}
-
-function vectorDiff(v1,v2) {
-    var result = [];
-    v1.forEach(function(item,i) {
-        result[i] = v1[i] - v2[i];
+    this.neurons.forEach(function(item, i) {
+      resVector.push(item.process(vector, self.distributionMatrix[i]));
     });
-    return result;
+    return resVector;
+  };
+  this.modifyWeights = function(input, errors, teta) {
+    var self = this;
+    input.forEach(function(item) {
+      while (item.length < self.inVectorLength) {
+        item.unshift(1);
+      }
+    });
+    errors.forEach(function(error, i) {
+      self.distributionMatrix.forEach(function(elem, j) {
+        elem.forEach(function(oldWeight, k) {
+          self.distributionMatrix[j][k] = oldWeight +
+          teta * error[0] * input[i][k];
+        });
+      });
+    });
+  };
+  return this;
+};
+
+function vectorDiff(v1, v2) {
+  var result = [];
+  v1.forEach(function(item, i) {
+    result[i] = v1[i] - v2[i];
+  });
+  return result;
 }
 
 function sumVector(vector) {
-    sum = 0;
-    vector.forEach(function(item) {
-        sum += item;
-    });
-    return sum;
+  sum = 0;
+  vector.forEach(function(item) {
+    sum += item;
+  });
+  return sum;
 }
 
 function sumVectorOfVector(vVector) {
-    var sum = 0;
-    vVector.forEach(function(item) {
-        sum += sumVector(item);
-    });
-    return sum;
+  var sum = 0;
+  vVector.forEach(function(item) {
+    sum += sumVector(item);
+  });
+  return sum;
 }
 
 function vectorNumberMultiply(vector, number) {
-    var res = vector.map(function(item) { return item * number; });
-    return res;
+  var res = vector.map(function(item) { return item * number; });
+  return res;
 }
 
 function vectorMultiply(v1, v2) {
-    var res = 0;
-    v1.forEach(function(item, i) {
-        res += item  * v2[i];
-    });
-    return res;
+  var res = 0;
+  v1.forEach(function(item, i) {
+    res += item  * v2[i];
+  });
+  return res;
 }
 
-function checkVVForNull(vVector){
-    var sum = 0;
-    vVector.forEach(function(item) {
-        if(sumVector(item)!=0) {
-            return false;
-        }
-    });
-    return true;
+function checkVVForNull(vVector) {
+  var sum = 0;
+  var flag = true;
+  vVector.forEach(function(item) {
+    if (sumVector(item) !== 0) {
+      flag = false;
+    }
+  });
+  return flag;
 }
 
 function train(perceptron, trainingSet) {
+  while(1) {
     var result = [];
     var trueResult = [];
     var input = [];
+    perceptron.counter++;
+    if(perceptron.counter > 1000) {
+      debugger;
+    }
     trainingSet.forEach(function(item, i) {
-        var output = perceptron.process(item.in);
-        input[i] = item.in;
-        result[i]  = output;
-        trueResult[i] = [item.out];
+      var output = perceptron.process(item.in);
+      input[i] = item.in;
+      result[i]  = output;
+      trueResult[i] = [item.out];
     });
     var errorVector = [];
-    trueResult.forEach(function(item,i) {
-        errorVector.push(vectorDiff(item, result[i]));
+    trueResult.forEach(function(item, i) {
+      errorVector.push(vectorDiff(item, result[i]));
     });
     var teta = 0.5;
-    if(!checkVVForNull(errorVector)) {
-        perceptron.modifyWeights(input, errorVector, teta);
-        train(perceptron, trainingSet);
+    if (!checkVVForNull(errorVector)) {
+      perceptron.modifyWeights(input, errorVector, teta);
     } else {
-        return perceptron;
+      return perceptron;
     }
+  }
 }
